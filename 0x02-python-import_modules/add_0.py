c7e79aa62e09795db1f816d9f7c0e88177b5b2a6
chiderafaith <faithjosephs114@gmail.com>
2022-09-25 17:01:29 -0700

    correction

--- 0x02-python-import_modules/add_0.py
+++ 0x02-python-import_modules/add_0.py
@@ -3,10 +3,9 @@ def add(a, b):
     """My addition function
 
     Args:
-     a: first integer
-     b: second integer
-
-     Returns:
-     The return value. a + b
-     """
-     return (a + b)
+    a: first integer
+    b: second integer
+    Returns:
+    The return value. a + b
+    """
+    return (a + b)
