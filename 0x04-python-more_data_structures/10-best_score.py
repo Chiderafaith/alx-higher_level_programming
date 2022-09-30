fdf1b6efa5144ea3bf7d98bc43edb72e7a693979
chiderafaith <faithjosephs114@gmail.com>
2022-09-30 16:35:38 -0700

    correction task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,6 +5,6 @@ def best_score(my_dict):
         for key in my_dict:
             if my_dict[key] > my_dict[max]:
                 max = key
-            return max
-        return None
+        return max
+    return None
 
